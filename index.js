4702251723b7567d9de12f1bf6b4ea301dae2124
Prem Rajput <104985726+premrajput4501@users.noreply.github.com>
2024-08-01 19:38:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ var randomnumber1=Math.floor(Math.random()*6)+1; //generating random number 1-6
 
 var randomnumber2 = Math.floor(Math.random()*6)+1;
 
+document.querySelectorAll("p")[1].innerHTML = "Turn - Player 1";
+document.querySelectorAll("p")[1].style.color="cyan";
+document.querySelectorAll("p")[1].style.textDecoration="underline";
+
 function roll1(){
 
         var randomImgSource = "images/dice"+randomnumber1+".png"; // /images/dice-n.png string generated
         // console.log(randomImgSource);
 
-        var img1= document.querySelectorAll("img")[0]; //creating a selector to easily select the first img tag with index 0
+        
 
-        img1.setAttribute("src",randomImgSource);
+        document.querySelectorAll("img")[0].setAttribute("src",randomImgSource); //creating a selector to easily select the first img tag with index 0
 
-        document.querySelectorAll("p")[1].innerHTML = "Player 1 rolls " +randomnumber1; 
+        document.querySelectorAll("p")[1].innerHTML = "Player 1 rolls - " +randomnumber1; 
+        document.querySelectorAll("p")[1].style.textDecoration="none";
+        document.querySelectorAll("p")[1].style.color="#32ffbb";
+
+        document.querySelectorAll("img")[1].addEventListener("click",roll2);
+
+        document.querySelectorAll("p")[2].innerHTML = "Turn - Player 2"; 
+        document.querySelectorAll("p")[2].style.color="cyan";
+        document.querySelectorAll("p")[2].style.textDecoration="underline";
 
 }
 
@@ -23,25 +35,20 @@ function roll2(){
 
         document.querySelectorAll("img")[1].setAttribute("src",randomImgSource2);
 
-        document.querySelectorAll("p")[2].innerHTML = "Player 2 rolls " +randomnumber2; 
+        document.querySelectorAll("p")[2].innerHTML = "Player 2 rolls - " +randomnumber2;
+        document.querySelectorAll("p")[2].style.textDecoration="none";
+        document.querySelectorAll("p")[2].style.color="#32ffbb";
 
         if (randomnumber1 > randomnumber2){
             document.querySelector("h1").innerHTML= "Player 1 wins!"
             document.querySelector("h1").style.color="#00ff1a"
-            console.log(randomnumber1);
-            console.log("p1 wins")
         }
         else if (randomnumber2 > randomnumber1){
             document.querySelector("h1").innerHTML= "Player 2 wins!"
             document.querySelector("h1").style.color="#00ff1a"
-            console.log(randomnumber2);
-            console.log("p2 wins")
         } else {
             document.querySelector("h1").innerHTML= "Draw : ("
-            document.querySelector("h1").style.color="white"
-            console.log("draw")
-            console.log(randomnumber1);
-            console.log(randomnumber2);
+            document.querySelector("h1").style.color="grey"
         }
 
         document.querySelector("button").style.visibility="visible";
