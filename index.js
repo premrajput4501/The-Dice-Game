eb8e50994585e5a5fc20383291614a6267a9b708
Prem Rajput <104985726+premrajput4501@users.noreply.github.com>
2024-08-07 13:09:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ document.querySelectorAll("p")[1].innerHTML = "Turn - Player 1";
 document.querySelectorAll("p")[1].style.color="cyan";
 document.querySelectorAll("p")[1].style.textDecoration="underline";
 
+document.querySelectorAll("img")[0].addEventListener("click",roll1);
+
 function roll1(){
 
         var randomImgSource = "images/dice"+randomnumber1+".png"; // /images/dice-n.png string generated
-        // console.log(randomImgSource);
-
-        
 
         document.querySelectorAll("img")[0].setAttribute("src",randomImgSource); //creating a selector to easily select the first img tag with index 0
 
         document.querySelectorAll("p")[1].innerHTML = "Player 1 rolls - " +randomnumber1; 
         document.querySelectorAll("p")[1].style.textDecoration="none";
-        document.querySelectorAll("p")[1].style.color="#32ffbb";
+        document.querySelectorAll("p")[1].style.color="blue";
 
-        document.querySelectorAll("img")[1].addEventListener("click",roll2);
+        document.querySelectorAll("img")[0].removeEventListener("click",roll1);
 
         document.querySelectorAll("p")[2].innerHTML = "Turn - Player 2"; 
         document.querySelectorAll("p")[2].style.color="cyan";
         document.querySelectorAll("p")[2].style.textDecoration="underline";
+
+        document.querySelectorAll("img")[1].addEventListener("click",roll2);
+
 
 }
 
@@ -37,7 +39,8 @@ function roll2(){
 
         document.querySelectorAll("p")[2].innerHTML = "Player 2 rolls - " +randomnumber2;
         document.querySelectorAll("p")[2].style.textDecoration="none";
-        document.querySelectorAll("p")[2].style.color="#32ffbb";
+        document.querySelectorAll("p")[2].style.color="blue";
+        
 
         if (randomnumber1 > randomnumber2){
             document.querySelector("h1").innerHTML= "Player 1 wins!"
@@ -51,13 +54,15 @@ function roll2(){
             document.querySelector("h1").style.color="grey"
         }
 
-        document.querySelector("button").style.visibility="visible";
+        document.querySelector(".restart-button").style.visibility="visible";
 
 }
 
 function restart(){
     location.reload();
 }
+
+
 
 
 
